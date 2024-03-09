a2f13faecfbce3199e6569fc9bfc6543d2d0df86
imranmiskat89 <imranmiskat89@gmail.com>
2024-03-09 21:06:56 +0600

    add backend code

---
+++ backend/dto/comment.js
@@ -0,0 +1,17 @@
+const commentController = require("../controller/commentController")
+
+
+class CommentDTO{
+    constructor(comment){
+        this._id = comment._id
+        this.createdAt = comment.createdAt
+        this.content = comment.content
+        this.authorUsername = comment.author.username
+
+    }
+}
+
+
+
+module.exports = CommentDTO
+
---
+++ backend/routes/index.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const authController = require("../controller/authController")
+const auth = require("../middlewares/auth")
+const blogController = require("../controller/blogController")
+const commentController = require("../controller/commentController")
+
+const router = express.Router()
+
+router.post("/register", authController.register)
+
+router.post("/login", authController.login)
+
+router.post("/logout", auth, authController.logout)
+
+router.get("/refresh", authController.refresh)
+
+router.post("/blog", auth, blogController.create)
+
+router.get("/blog/all", auth, blogController.getAll)
+
+router.get("/blog/:id", auth, blogController.getById)
+
+router.put("/blog", auth, blogController.update)
+
+router.delete("/blog/:id", auth, blogController.delete)
+
+router.post("/comment", auth, commentController.create)
+
+router.get("/comment/:id", auth, commentController.getById)
+
+
+
+module.exports = router
+
